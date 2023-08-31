f67c70d6d75309b4e74e772a1d25a71674da2fd7
gabutriveni <gabutriveni@gmail.com>
2023-08-31 10:39:48 +0530

    Footer.js

---
+++ Footer.js
@@ -0,0 +1,92 @@
+import "./Footer.css";
+const Footer= ()=>{
+    return(
+        <div className="footer">
+            <div className="top">
+                <div>
+                    <h1>Red Bus</h1>
+                    <p>Choose your favourite destinaction</p>
+                    <img
+              src="https://st.redbus.in/Images/rdc/rdc-redbus-logo.svg"alt=""/>
+                </div>
+                <div>
+                    <a href="/">
+                        <i className="fa-brands fa-facebook-square"></i>
+                    </a>
+                    <a href="/">
+                        <i className="fa-brands fa-twitter-square"></i>
+                    </a>
+                    <a href="/">
+                        <i className="fa-brands fa-instagram-square"></i>
+                    </a>
+                    <a href="/">
+                        <i className="fa-brands fa-behance-square"></i>
+                    </a>
+                    <a href="/">
+                        <i className="fa-brands fa-whatsapp-square"></i>
+                    </a>
+                    <a href="/">
+                        <i className="fa-brands fa-youtube-square"></i>
+                    </a>
+                    <a href="/">
+                        <i className="fa-brands fa-linkedin-square"></i>
+                    </a>
+                </div>
+            </div>
+            <div className="bottom">
+                <div>
+                    <h4>About Redbus</h4>
+                    <a href="/">About us</a>
+                    <a href="/">Contact Us</a>
+                    <a href="/">Mobile Version</a>
+                    <a href="/">Red bus on mobile</a>
+                    <a href="/">Offers</a>
+                    <a href="/">Careers</a>
+                    <a href="/">Values</a>
+                </div>
+                <div>
+                    <h4>Info</h4>
+                    <a href="/">Terms and conditions</a>
+                    <a href="/">Privacy Policy</a>
+                    <a href="/">Natere</a>
+                    <a href="/">FAQ</a>
+                    <a href="/">Blog</a>
+                    <a href="/">Bus Operator</a>
+                    <a href="/">Registeration</a>
+                    <a href="/">Agent Registeration</a>
+                    <a href="/">Insurance Partnerartner</a>
+                   
+                </div>
+                <div>
+                    <h4>Global Sites</h4>
+                    <a href="/">India</a>
+                    <a href="/">Singapore</a>
+                    <a href="/">Malatysia</a>
+                    <a href="/">Peru</a>
+                    <a href="/">Columbia</a>
+                </div>
+                <div>
+                    <h4>Our Partners</h4>
+                    <a href="/">Goibibo Hotels</a>
+                    <a href="/">Goibibo Bus</a>
+                    <a href="/">Makemytrip Bus</a>
+                    <a href="/">Makemy trip hotels</a>
+                    <a href="/">Find a Travel Agent</a>
+                </div>
+              
+              <p>redBus is the worlds largest online bus <br/>
+                ticket booking service trusted by over<br/> 
+                8 million happy customers globally.<br/>redBus offers bus ticket booking<br/> 
+                through its website,iOS and Android<br/> mobile apps for all major routes. </p>
+                <p>
+          Ⓒ 2023 Redbus India Private Limited <br/>
+          (formerly known as ‘Ibibo Group
+          Private Limited’) All rights reserved
+        </p>
+            </div>
+
+        </div>
+
+    )
+}
+export default Footer;
